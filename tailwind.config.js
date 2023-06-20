78442b8c05d2ca82971a08214a9049b8f22debd7
Sanne Aarts <88075950+Snrts@users.noreply.github.com>
2023-06-20 13:31:29 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,23 @@ module.exports = {
       widest: '.1em',
       widest: '.25em',
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      '3xl': '1920px',
+    },
     extend: {
       
     },
