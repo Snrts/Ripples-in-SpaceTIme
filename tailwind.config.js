4d48db848e4ab7b5d4b39e1c3f8b9a19195672f1
Sanne Aarts <88075950+Snrts@users.noreply.github.com>
2023-07-02 14:41:43 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html", "./src/**/*.js", "node_modules/flowbite/**/*.js", "src/**/*.{html,js}"],
+  content: ["*.html", "./src/**/*.js", "/node_modules/flowbite/**/*.js", "src/**/*.{html,js}"],
   theme: {
     colors: {
       transparent: "transparent",
